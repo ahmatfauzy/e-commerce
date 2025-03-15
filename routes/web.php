cab954b90bbc51bd3abbda954cc6e75c699b92ca
ahmatfauzy <ahmatf703@gmail.com>
2025-03-15 14:30:50 +0700

    [Pertemuan 1] Installasi dan Pengenalan Laravel

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::get('/', function () {
+    return('Halaman utama');
+});
+
+Route::get('/categories', function () {
+    return('halaman kategori dalam toko');
+});
+
+
+Route::get('/orders', function () {
+    return('Halaman checkout');
+});
+
+Route::get('/payments', function () {
+    return('Halaman pembayaran');
+});
+
+
+Route::get('/reviews', function () {
+    return('Halaman review');
+});
+
+Route::get('/promos', function () {
+    return('Halaman promo');
+});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
+
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
+
+// Route::middleware(['auth'])->group(function () {
+//     Route::redirect('settings', 'settings/profile');
+
+//     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+//     Volt::route('settings/password', 'settings.password')->name('settings.password');
+//     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+// });
+
+require __DIR__.'/auth.php';
