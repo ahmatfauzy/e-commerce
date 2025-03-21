053d622103f1930b2086a19c4eb99438492b738c
ahmatfauzy <ahmatf703@gmail.com>
2025-03-21 12:55:54 +0700

    [Pertemuan 3] Membuat Layout dan Component

---
+++ resources/views/web/products.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3>Ini adalah halaman product</h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,41 +3,74 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return('Halaman utama');
-});
-
-Route::get('/categories', function () {
-    return('halaman kategori dalam toko');
-});
-
-
-Route::get('/orders', function () {
-    return('Halaman checkout');
-});
-
-Route::get('/payments', function () {
-    return('Halaman pembayaran');
-});
-
-
-Route::get('/reviews', function () {
-    return('Halaman review');
-});
-
-Route::get('/promos', function () {
-    return('Halaman promo');
-});
-
-Route::get('/products', function () {
-    return('Halaman daftar produk');
-});
-
-Route::get('/products/{id}', function ($id) {
-    return "Detail produk dengan ID: $id";
-});
-
 // Route::get('/', function () {
+//     return view('web.homepage');
+// });
+
+Route::get('/', function () {
+    $title = "Homepage";
+    return view('web.homepage', ['title' => $title]);
+});
+
+
+Route::get('products', function () {
+    $title = "Products";
+    return view("web.products", ['title' => $title]);
+});
+
+Route::get('product/{slug}', function ($slug) {
+    return "halaman single product - " . $slug;
+});
+
+Route::get('categories', function () {
+    return view("web.categories");
+});
+
+Route::get('category/{slug}', function ($slug) {
+    return "halaman single category - " . $slug;
+});
+
+Route::get('cart', function () {
+    $title = "Cart";
+    return view("web.cart", ['title' => $title]);
+});
+
+Route::get('checkout', function () {
+    $title = "co";
+    return view("web.checkout", ['title' => $title]);
+});
+
+
+// Route::get('/categories', function () {
+//     return('halaman kategori dalam toko');
+// });
+
+
+// Route::get('/orders', function () {
+//     return('Halaman checkout');
+// });
+
+// Route::get('/payments', function () {
+//     return('Halaman pembayaran');
+// });
+
+// Route::get('/reviews', function () {
+//     return('Halaman review');
+// });
+
+// Route::get('/promos', function () {
+//     return('Halaman promo');
+// });
+
+// Route::get('/products', function () {
+//     return('Halaman daftar produk');
+// });
+
+// Route::get('/products/{id}', function ($id) {
+//     return "Detail produk dengan ID: $id";
+// });
+
+// Route::get('/welcome', function () {
 //     return view('welcome');
 // })->name('home');
 
@@ -53,4 +86,4 @@ Route::get('/products/{id}', function ($id) {
 //     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 // });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
